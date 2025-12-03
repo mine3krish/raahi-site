ad6d9bb38470b0c016fe4afabf384081d0e8762f
unknown <mine3krish@gmail.com>
2025-12-03 14:55:48 +0530

    3rd commit

--- app/page.tsx
+++ app/page.tsx
@@ -27,9 +27,9 @@ export default function Home() {
   return (
     <div>
       <Hero/>
+      <PropertyScroller />
       <WhyChooseUs />
       <JoinCommunity />
-      <PropertyScroller />
       <IndiaMapSection />
       <StateWiseProperties />
     </div>
